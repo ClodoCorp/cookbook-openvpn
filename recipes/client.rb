bc902aaac6aa185f97e19a96e6fdc9cc60cc3525
vadikgo <vadikgo@gmail.com>
2015-08-12 15:00:45 +0300

    add systemd support

--- recipes/client.rb
+++ recipes/client.rb
@@ -6,3 +6,18 @@ service "openvpn" do
   action [:enable, :start]
   reload_command "/etc/init.d/openvpn soft-restart"
 end
+
+class << self; include OpenvpnHelpers; end
+
+openvpn_process :client_configs do
+    config_name = self.conf_name
+    config = self.conf
+    # user_name required for given vpn server/config
+    user_name = config[:user_name]
+
+    service "openvpn@#{config_name}-#{user_name}" do
+      action [:enable, :start]
+      reload_command "/etc/init.d/openvpn soft-restart"
+      only_if { ::File.exists?('/lib/systemd/system/openvpn@.service') }
+    end
+end
